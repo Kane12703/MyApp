fb654813a20378c398a0e94a48ca626316fcbf45
Kane12703 <117529368+Kane12703@users.noreply.github.com>
2023-07-07 14:57:07 +0700

    a

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import AllProduct from './src/app/book/screens/AllProduct';
 import Register from './src/app/users/screens/Register';
 import UserNavigation from './src/app/users/navigation/UserNavigation';
 import {NavigationContainer} from '@react-navigation/native';
-import Test from './src/app/book/screens/Test';
+
 
 
 
